4336febf9ae1b7b938c0f4bed8cdf9e382d204a8
Nicolas Gallardo <gallardo.3853@gmail.com>
2022-09-23 20:31:29 -0300

    23/09/22

--- Cadenas2.py
+++ Cadenas2.py
@@ -24,4 +24,18 @@
 #     if (stringSinEspacios[longitudString - 1] != stringAlReves[longitudString - 1]):
 #         print("No son palíndromos")
 
+# Arnaldo pasa por una concesionaria para averiguar por un Fiat 600 0 km. Él posee una cantidad de dinero disponible para comprarlo. Si el auto sale menos de lo que tiene lo compra directamente. Si no, si le alcanza aunque sea para la mitad paga todo lo tenga y la concesionaria le financia lo faltante con un %10 de recargo. Arnaldo accede a este préstamo si el dinero que tiene que agregar no supera los $10000. Se pide hacer un programa que diga que va a realizar Arnaldo y en caso de comprar indique cuanto paga en total.
 
+dineroDisponible = int(input("Ingrese el dinero disponible: "))
+precioAuto = int(input("Ingrese el precio del auto: "))
+
+if (dineroDisponible >= precioAuto):
+    print("Arnaldo paga el total")
+if (dineroDisponible < precioAuto):
+    tasaFinanciamiento = 0.1
+    capitalFinanciado = (precioAuto - dineroDisponible)* (1.0 + tasaFinanciamiento)
+    if (dineroDisponible > (precioAuto / 2) and capitalFinanciado <= 10000):
+        print("Arnaldo se financia con: ", capitalFinanciado)
+        print("Arnaldo paga: ", dineroDisponible)
+    else:
+        print("Arnaldo no puede comprar el auto")
