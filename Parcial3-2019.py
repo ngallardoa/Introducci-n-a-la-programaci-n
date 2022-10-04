64f9130ac7765c24fcf84b8855ce3e7bd1c9e602
Nicolas Gallardo <gallardo.3853@gmail.com>
2022-10-04 15:32:33 -0300

    04/10/22

--- Parcial3-2019.py
+++ Parcial3-2019.py
@@ -2,38 +2,43 @@ from random import *
 
 stringAAnalizar = input("Ingrese una palabra: ")
 stringAAnalizarSinPrimerCaracter = ""
+string0 = ""
 string1 = ""
-string2 = ""
-caracteresRepetidos0 = stringAAnalizar[0]
+caracteresRepetidos0 = ""
 caracteresRepetidos1 = ""
 i = 1
 
-while (i < len(stringAAnalizar)):
-    stringAAnalizarSinPrimerCaracter += stringAAnalizar[i]
-    i += 1
+if (randrange(2) == 0): # Asigna el primer caracter del string de forma pseudoaleatoria a alguno de los substrings
+    string0 += stringAAnalizar[0]
+else:
+    string1 += stringAAnalizar[0]
 
-i = 0
-
-while (i < len(stringAAnalizar)): # ciclo para asignar cada una de las letras a los strings
-    if stringAAnalizar[i] in caracteresRepetidos0:
+while (i < len(stringAAnalizar)): # Ciclo para asignar cada una de las letras a los substrings de forma pseudoaleatoria
+    print(stringAAnalizar[i])
+    if stringAAnalizar[i] in caracteresRepetidos0 or stringAAnalizar[i] == stringAAnalizar[0]:
         caracteresRepetidos1 += stringAAnalizar[i]
     else:
         if (randrange(2) == 0):
-            string1 += stringAAnalizar[i]
+            string0 += stringAAnalizar[i]
         else:
-            string2 += stringAAnalizar[i]
+            string1 += stringAAnalizar[i]
         caracteresRepetidos0 += stringAAnalizar[i]
     i += 1
 
-caracteresRepetidos0 = ""
+difLen = len(string0) - len(string1)
 
-if caracteresRepetidos1[0] not in stringAAnalizarSinPrimerCaracter: # como la primera letra del string ingresado por el usuario nunca se analizó si se repite, este ciclo realiza esta operación, y en caso de no estar repetido, lo elimina del string caracteresRepetidos0 (el string que guarda los caracteres repetidos) e imprime este.
-    for i in range(1, len(caracteresRepetidos1)): 
-        caracteresRepetidos0 += caracteresRepetidos1[i]
-    print("Los caracteres repetidos son: ", caracteresRepetidos0)
-    print("string1", string1)
-    print("string1", string2)
-else:
-    print("Los caracteres repetidos son: ", caracteresRepetidos1)
-    print("string1", string1)
-    print("string1", string2)
+if (difLen < 0): # Si string0 es más corto que string1, entra en este condicional para completarlo con "&" hasta igualar longitudes
+    while (difLen != 0):
+        string0 += "&"
+        difLen += 1
+if (difLen > 0): # Si string1 es más corto que string0, entra en este condicional para completarlo con "$" hasta igualar longitudes
+    string1Auxiliar = ""
+    while (difLen != 0):
+        string1Auxiliar += "$"
+        difLen -= 1
+    string1Auxiliar += string1
+    string1 = string1Auxiliar
+
+print("Los caracteres repetidos son: ", caracteresRepetidos1)
+print("string0", string0)
+print("string1", string1)
